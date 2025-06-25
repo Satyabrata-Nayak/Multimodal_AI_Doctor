44c3df640031d2de8d8d376c9989616737af7845
Satyabrata-Nayak <140244874+Satyabrata-Nayak@users.noreply.github.com>
2025-06-25 20:09:28 +0530

    changed api_key

--- voice_of_doctor.py
+++ voice_of_doctor.py
@@ -77,7 +77,8 @@ def text_to_speech_with_gtts(input_text, output_filepath):
 
 
 def text_to_speech_with_elevenlabs(input_text, output_filepath):
-    client = ElevenLabs(api_key=ELEVEN_API_KEY)
+    api_key = os.getenv("ELEVEN_API_KEY")
+    client = ElevenLabs(api_key=api_key)
 
     audio = client.text_to_speech.convert(
         text=input_text,
