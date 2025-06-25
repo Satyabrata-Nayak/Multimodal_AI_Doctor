128cbc7cca291a9f4f8d22466e776244955781b4
Satyabrata-Nayak <satyabrata@kgpian.iitkgp.ac.in>
2025-06-25 19:29:36 +0530

    second commit

--- voice_of_doctor.py
+++ voice_of_doctor.py
@@ -61,32 +61,50 @@ def text_to_speech_with_gtts(input_text, output_filepath):
 #text_to_speech_with_gtts(input_text=input_text, output_filepath="gtts_testing_autoplay.mp3")
 
 
+def text_to_speech_with_gtts(input_text, output_filepath):
+    language = "en"
+    audioobj = gTTS(text=input_text, lang=language, slow=False)
+    audioobj.save(output_filepath)
+
+    # Play audio only if not on Render
+    if not os.getenv("RENDER"):
+        try:
+            subprocess.run(['ffplay', '-nodisp', '-autoexit', output_filepath])
+        except Exception as e:
+            print(f"An error occurred while trying to play the audio: {e}")
+
+    return output_filepath
+
+
 def text_to_speech_with_elevenlabs(input_text, output_filepath):
-    client=ElevenLabs(api_key=ELEVENLABS_API_KEY)
-    audio=client.text_to_speech.convert(
-        text= input_text,
+    client = ElevenLabs(api_key=ELEVENLABS_API_KEY)
+
+    audio = client.text_to_speech.convert(
+        text=input_text,
         voice_id="SV61h9yhBg4i91KIBwdz",
-        output_format= "mp3_22050_32",
-        model_id= "eleven_turbo_v2"
+        output_format="mp3_22050_32",
+        model_id="eleven_turbo_v2"
     )
     elevenlabs.save(audio, output_filepath)
-    try:
-        subprocess.run(['ffplay', '-nodisp', '-autoexit', output_filepath])
-    except Exception as e:
-        print(f"An error occurred while trying to play the audio: {e}")
 
-# text_to_speech_with_elevenlabs(input_text, output_filepath="elevenlabs_testing_autoplay.mp3")
+    # Play audio only if not on Render
+    if not os.getenv("RENDER"):
+        try:
+            subprocess.run(['ffplay', '-nodisp', '-autoexit', output_filepath])
+        except Exception as e:
+            print(f"An error occurred while trying to play the audio: {e}")
+
+    return output_filepath
+
 
 def text_to_speech_with_fallback(input_text, output_filepath="final.mp3"):
     try:
-        # First attempt: ElevenLabs
         return text_to_speech_with_elevenlabs(
             input_text=input_text,
             output_filepath=output_filepath
         )
     except Exception as e:
         print(f"ElevenLabs failed ({str(e)}). Falling back to gTTS")
-        # Fallback to Google TTS
         return text_to_speech_with_gtts(
             input_text=input_text,
             output_filepath=output_filepath
